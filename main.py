84f81a851fc07dbaf59a33f9a950b8a778d25672
atrox3d <atrox3d@gmail.com>
2022-12-29 21:40:32 +0100

    354. Credit Card feature

--- main.py
+++ main.py
@@ -37,18 +37,40 @@ class ReservationTicket:
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card = dict(
+            number=self.number,
+            expiration=expiration,
+            holder=holder,
+            cvc=cvc
+        )
+        if card in df_cards:
+            return True
+        else:
+            return False
+
 if __name__ == '__main__':
     df = pd.read_csv('hotels.csv', dtype={'id': str})
+    df_cards = pd.read_csv('cards.csv', dtype=str).to_dict(orient='records')
     # print(df.info())
     print(df.to_string(index=False))
     hotel_id = input('Enter id of the hotel: ')
     hotel = Hotel(hotel_id=hotel_id)
     if hotel.available():
-        hotel.book()
-        name = input('Enter your name: ')
-        ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
-        receipt = ticket.generate()
-        print(receipt)
+        # card_number = input('Enter credit card number: ')
+        card = CreditCard(number='1234')
+        if card.validate(expiration='12/26', holder='JOHN SMITH', cvc='123'):
+            hotel.book()
+            name = input('Enter your name: ')
+            ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+            receipt = ticket.generate()
+            print(receipt)
+        else:
+            print('there was a problem with your payment')
     else:
         print('hotel is not free')
 
